b828bd5f7fa17596557793019bbde1f366cde788
Rory Stokes <rstokes@spartaglobal.com>
2019-11-01 13:49:58 +0000

    ubuntu version changed

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,10 +7,10 @@
 require 'spec_helper'
 
 describe 'mongo::default' do
-  context 'When all attributes are default, on Ubuntu 18.04' do
+  context 'When all attributes are default, on Ubuntu 16.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
+    platform 'ubuntu', '16.04'
 
     it 'Should update source list' do
       expect(chef_run).to update_apt_update 'update_sources'
